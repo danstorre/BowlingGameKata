94cd137a19f453509db09fd35d2dcd55f2e48d3a
Dan Torres <danstorre@gmail.com>
2023-08-18 11:39:17 -0400

    on gutter game, score delivers 0

--- BowlingGameTests/BowlingGameTests.swift
+++ BowlingGameTests/BowlingGameTests.swift
@@ -1,36 +1,20 @@
-//
-//  BowlingGameTests.swift
-//  BowlingGameTests
-//
-//  Created by Daniel Torres on 8/18/23.
-//
-
 import XCTest
-@testable import BowlingGame
+
+final class Game {
+    func roll(_ pins: Int) {
+    }
+    
+    func score() -> Int {
+        return 0
+    }
+}
 
 final class BowlingGameTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGutterGame() {
+        let game = Game()
+        for _ in 1...20 {
+            game.roll(0)
         }
+        XCTAssertEqual(game.score(), 0)
     }
-
 }
