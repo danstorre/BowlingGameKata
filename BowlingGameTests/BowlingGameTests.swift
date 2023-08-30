f44c427fc292ba3be19d094bf82e6b683f63cc96
Dan Torres <danstorre@gmail.com>
2023-08-30 14:11:52 -0400

    on gutter game should deliver score of zero.

--- BowlingGameTests/BowlingGameTests.swift
+++ BowlingGameTests/BowlingGameTests.swift
@@ -2,11 +2,25 @@ import XCTest
 
 class BowlingGame {
     let score: Int = 0
+    
+    func roll(pinsKnocked: Int) {
+        
+    }
 }
 
 final class BowlingGameTests: XCTestCase {
-    func test_score_shouldDeliverZero() {
+    func test_score_shouldDeliverScoreOfZero() {
         let sut = BowlingGame()
+        XCTAssertEqual(sut.score, 0)
+    }
+    
+    func test_roll_onGutterGame_shouldDeliverScoreOfZero() {
+        let sut = BowlingGame()
+        
+        Array<Int>.init(repeating: 0, count: 20).forEach { roll in
+            sut.roll(pinsKnocked: 0)
+        }
+        
         XCTAssertEqual(sut.score, 0)
     }
 }
