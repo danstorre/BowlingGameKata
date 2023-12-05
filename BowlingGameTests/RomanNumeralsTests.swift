1d9f8215304f9998091df21b562c4fb143b186e1
Peter Combee <petercombee@gmail.com>
2023-12-05 20:20:36 +0100

    Rename function to `toDecimalString`

--- BowlingGameTests/RomanNumeralsTests.swift
+++ BowlingGameTests/RomanNumeralsTests.swift
@@ -2,13 +2,13 @@
 import XCTest
 
 enum RomanNumeralConverter {
-    static func convert(_ int: Int) -> String { "I" }
+    static func toDecimalString(_ int: Int) -> String { "I" }
 }
 
 final class RomanNumeralsTests: XCTestCase {
     
     func test_delivers_I_for_1() {
-        let result = RomanNumeralConverter.convert(1)
+        let result = RomanNumeralConverter.toDecimalString(1)
 
         XCTAssertEqual(result, "I")
     }
